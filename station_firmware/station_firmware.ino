38f521c944370dfc4befb42b5d4f6cf4bc26c582
Arthur Castro <arthur.castro07@gmail.com>
2022-10-18 21:55:04 -0300

    Integrando leitura do Sensor MQ-2 ao código principal

--- station_firmware/station_firmware.ino
+++ station_firmware/station_firmware.ino
@@ -10,18 +10,15 @@
 #include <PubSubClient.h>
 #include <LiquidCrystal_I2C.h>
 
-
 #include "env.h"
 
+
 // ---------- Diretivas ----------
-// Hardware
-#define GAS_SENSOR_PIN    34
+// Debug
+#define DEBUG_SENSOR_MQ2
+
+// Sirene
 #define LED_PIN           2
-
-#define CRITICAL_GAS_VALUE            60
-
-#define GAS_PUBLISH_INTERVAL          5000
-#define GAS_CRITICAL_ALERT_INTERVAL   30000
 
 //LCD
 #define LCD_COLUMNS   16
@@ -37,10 +34,19 @@
 #define TOPIC_GAS_SENSOR_VALUE      "uPProject/station/1/mq2/value"
 #define TOPIC_GAS_SENSOR_ALERT      "uPProject/station/1/mq2/alert"
 
+// Sensor MQ-2
+#define MQ2_SENSOR_PIN                  34
+#define ADC_RESOLUTION                  4095
+#define INTERVAL_MS_BETWEEN_READINGS    10
+#define NUMBER_OF_READINGS              100
+
+#define CRITICAL_GAS_VALUE            10
+
+#define GAS_PUBLISH_INTERVAL          5000
+#define GAS_CRITICAL_ALERT_INTERVAL   30000
 
 // ---------- Declaracao de variaveis ----------
-// Sensor de gas
-float gasSensorValue = 0, gasSensorValuePercent = 0;
+long previousTime = 0, previousTime2 = 0;
 
 // WiFi
 const char* ssid = ENV_SSID;
@@ -48,8 +54,12 @@ const char* password = ENV_PASSWORD;
 
 // MQTT
 const char* mqtt_server = MQTT_BROKER_ADDR;     // Endereco do Broker MQTT
-long previousTime = 0, previousTime2 = 0;
 bool alreadyResetAlert = false;
+
+// Sensor MQ-2
+unsigned int mq2SumValue = 0, reading = 0;
+float mq2ValuePercent = 0, mq2Average = 0, lastMq2ValuePercent = 0;
+
 
 // ---------- Instanciando objetos ----------
 WiFiClient espClient;
@@ -62,6 +72,7 @@ LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_ROWS);
 void setupWifi();
 void callback(char*, byte*, unsigned int);
 void reconnect();
+void leituraDoSensorMQ2();
 
 void controlaLed(String);
 void trataLcd(String);
@@ -71,8 +82,8 @@ void trataLcd(String);
 void setup() {
   // Configura IOs
   pinMode(LED_PIN, OUTPUT);
-  pinMode(GAS_SENSOR_PIN, INPUT);
-  
+  pinMode(MQ2_SENSOR_PIN, INPUT);
+
   // Inicializando comunicacao serial
   Serial.begin(115200);
 
@@ -97,29 +108,29 @@ void loop() {
   }
   client.loop();
 
-  // Mantem conexao MQTT
-  long currentTime = millis();
-  if (currentTime - previousTime >= GAS_PUBLISH_INTERVAL) {
-    previousTime = currentTime;
-
-    // Leitura do sensor de gas
-    gasSensorValue = analogRead(GAS_SENSOR_PIN);
-    gasSensorValuePercent = map(gasSensorValue, 0, 4095, 0, 100);
+  leituraDoSensorMQ2();
+  
+  if (mq2ValuePercent != lastMq2ValuePercent) {
+    lastMq2ValuePercent = mq2ValuePercent;
     
     // Convertendo valor para um array de caracteres
-    char gasSensorValuePercentString[10];
-    dtostrf(gasSensorValuePercent, 5, 2, gasSensorValuePercentString);
+    char mq2ValuePercentString[10];
+    dtostrf(mq2ValuePercent, 5, 2, mq2ValuePercentString);
     
-    client.publish(TOPIC_GAS_SENSOR_VALUE, gasSensorValuePercentString);
-    Serial.print("Sensor de gás: ");
-    Serial.println(gasSensorValuePercentString);
+    client.publish(TOPIC_GAS_SENSOR_VALUE, mq2ValuePercentString);
+    Serial.print("Publicado valor do Sensor de gás: ");
+    Serial.println(mq2ValuePercentString);
     
-    if (gasSensorValuePercent >= CRITICAL_GAS_VALUE) {
+    if (mq2ValuePercent >= CRITICAL_GAS_VALUE) {
       alreadyResetAlert = false;
+
+      Serial.println("CRITICAL_GAS_VALUE");
+      Serial.print("previousTime2: ");
+      Serial.println(previousTime2);
       
-      long currentTime2 = millis();
-      if (currentTime2 - previousTime2 >= GAS_CRITICAL_ALERT_INTERVAL) {
-        previousTime2 = currentTime2;
+      
+      if (millis() - previousTime2 >= GAS_CRITICAL_ALERT_INTERVAL) {
+        previousTime2 = millis();
         
         client.publish(TOPIC_GAS_SENSOR_ALERT, "Perigo de incêndio!");
         Serial.println("Publicada mensagem de socorro");
@@ -221,6 +232,33 @@ void reconnect() {
       Serial.print(client.state());
       Serial.println(" nova tentativa em 5 segundos...");
       delay(5000);
+    }
+  }
+}
+
+void leituraDoSensorMQ2(){
+  if (reading >= NUMBER_OF_READINGS) {
+    reading = 0;
+
+    mq2Average = mq2SumValue / NUMBER_OF_READINGS;
+    mq2SumValue = 0;
+
+    mq2ValuePercent = map(mq2Average, 0, ADC_RESOLUTION, 0, 100);
+
+    #ifdef DEBUG_SENSOR_MQ2
+      Serial.print("[Sensor MQ-2] Média de leituras: ");
+      Serial.print(mq2ValuePercent);
+      Serial.println("%");
+    #endif
+  
+  } else {
+    
+    if (millis() - previousTime >= INTERVAL_MS_BETWEEN_READINGS) {
+      previousTime = millis();
+
+      mq2SumValue += analogRead(MQ2_SENSOR_PIN);
+
+      reading++;
     }
   }
 }
